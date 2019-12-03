b42b208b5eb0d9eca8b6101eaa6e00ad939ca02a
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2019-12-02 23:03:38 -0500

    Created knex config

---
+++ serverless/data-access/db/knexfile.js
@@ -0,0 +1,56 @@
+const config = require('../../config');
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      // port: process.env.DATABASE_PORT,
+      port: config.port,
+      host: config.host,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+    },
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
