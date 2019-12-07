b89d7dc8bd56f1a1b91751ea0ad7434d7e2cf6ea
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2019-12-07 09:53:26 -0500

    Created dest table/column migration and seed

---
+++ serverless/data-access/db/migrations/20191205235632_create-dest-table-column.js
@@ -0,0 +1,40 @@
+
+exports.up = async knex => {
+    await knex.raw('CREATE SCHEMA IF NOT EXISTS demo1');
+
+    await knex.schema.withSchema('demo1')
+    .createTable('destination_tables', function(dest_table) {
+        dest_table.increments('id').unsigned().primary();
+        dest_table.string('table_name', 200).notNull();
+        dest_table.boolean('valid').defaultTo(true);
+        dest_table.boolean('searchable').defaultTo(true);
+        dest_table.timestamp('created_at').defaultTo(knex.fn.now())
+    });
+
+    // .createTable('demo1.source_files', function(dest_table) {
+    //     dest_table.increments('id').unsigned().primary();
+    //     dest_table.string('source_name', 200).notNull();
+    //     dest_table.string('file_name_convention', 200).notNull();
+    //     dest_table.boolean('valid').defaultTo(true);
+    //     dest_table.boolean('searchable').defaultTo(true);
+    //     dest_table.timestamp('created_at').defaultTo(knex.fn.now())
+    // })
+
+    await knex.schema.withSchema('demo1')
+    .createTable('destination_columns', function(dest_column) {
+        dest_column.increments('id').unsigned().primary();
+        dest_column.integer('table_id').unsigned().notNull().references('id').inTable('demo1.destination_tables');
+        dest_column.string('column_name', 200).notNull();
+        dest_column.string('data_type', 50).notNull();
+        dest_column.boolean('required').defaultTo(false);
+        dest_column.timestamp('created_at').defaultTo(knex.fn.now());
+        dest_column.timestamp('modified_at').defaultTo(knex.fn.now());
+    });
+
+};
+
+exports.down = async knex => {
+    await knex.schema.withSchema('demo1').dropTable('destination_columns');
+    await knex.schema.withSchema('demo1').dropTable('destination_tables');
+    // .then(()=> knex.schema.withSchema('demo1').dropTable('destination_table'));
+};
