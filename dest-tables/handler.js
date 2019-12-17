8f1d65a839d212df77dcbf312676204f6510ad67
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2019-12-17 18:32:42 -0500

    Added publish entpoint to generate dest tables

--- dest-tables/handler.js
+++ dest-tables/handler.js
@@ -1,18 +1,25 @@
 'use strict';
 
 const { validateApiKey } = require('../src/services/users');
-const { insertTableColumnDefs } = require('../src/services/dest-tables')
+const { insertTableColumnDefs, generateTableIfNotExist } = require('../src/services/dest-tables');
 
-module.exports.saveTableColumnDefs = async (event, context) => {
+const saveTableColumnDefs = async (event, context) => {
 
   let clientCode = event.headers['client-code'];
   let apiKey = event.headers['x-api-key'];
-  let tableDef = JSON.parse(event.body);
+  let destTableId = event.pathParameters ? event.pathParameters.destTableId : null;
 
   let validationResult = await validateApiKey(clientCode, apiKey);
   if (!validationResult.valid) {
     const { valid, ...noA } = validationResult;
     return noA;
+  }
+
+  if (!destTableId) {
+    return {
+      statusCode: 400,
+      body: `destTableId is not known`
+    }
   }
 
   let error = await insertTableColumnDefs(clientCode, tableDef.tableName, tableDef.columnDefs);
@@ -29,4 +36,41 @@ module.exports.saveTableColumnDefs = async (event, context) => {
     };
   }
 
+};
+
+const publishTable = async (event, context) => {
+
+  let clientCode = event.headers['client-code'];
+  let apiKey = event.headers['x-api-key'];
+  let tableName = event.pathParameters ? event.pathParameters.tableName : null;
+
+  let validationResult = await validateApiKey(clientCode, apiKey);
+  if (!validationResult.valid) {
+    const { valid, ...noA } = validationResult;
+    return noA;
+  }
+
+  if (!tableName) {
+    return {
+      statusCode: 400,
+      body: `tableName is not known`
+    }
+  }
+
+  try {
+    let tableGenerated = await generateTableIfNotExist(clientCode, tableName);
+    let rtnBody = tableGenerated ? `${tableName} has been generated successfully` : `${tableName} already exists`;
+    return { statusCode: 200, body: rtnBody };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: `failed to generate table ${tableName}`
+    };
+  }
+
+};
+
+module.exports = {
+  saveTableColumnDefs,
+  publishTable
 };
