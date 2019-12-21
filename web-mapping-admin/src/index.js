608fa3863e65587aea2f649ca418dfc725e76fea
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2019-12-21 09:16:09 -0500

    Added by react-app-create

---
+++ web-mapping-admin/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import DestTable from './pages/dest-table/DestTable';
+import './index.css';
+
+ReactDOM.render(
+  <DestTable />,
+  document.getElementById('root')
+);
---
+++ web-mapping-admin/src/pages/dest-table/DestTable.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DestTable = (props) => {
+    return (
+        <div className="recommendations-page-container">
+            first paged
+        </div>
+    );
+}
+
+export default DestTable;
