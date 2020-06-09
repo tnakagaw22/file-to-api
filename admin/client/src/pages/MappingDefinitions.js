d7bdb736ddb917d5affd5a46efdcb6defb4dfe89
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2020-06-09 08:02:33 -0400

    updated ui after create new

--- admin/client/src/pages/MappingDefinitions.js
+++ admin/client/src/pages/MappingDefinitions.js
@@ -6,17 +6,41 @@ const MappingDefinitions = (props) => {
   const [srcFileName, setSrcFileName] = useState("");
   const [destTableName, setDestTableName] = useState("");
 
-  const { loading, error, data } = useQuery(GET_MAPPING_DEFINITIONS);
-  const [createMappingDefinition, { save_loading, save_error }] = useMutation(
-    CREATE_MAPPING_DEFINITION,
-    {
-      onCompleted({ newMappingDef }) {
-        console.log(`${newMappingDef} is created successfully`);
-        // localStorage.setItem('token', login as string);
-        // client.writeData({ data: { isLoggedIn: true } });
-      },
-    }
+  const { loading, error, data: resGetMds } = useQuery(
+    GET_MAPPING_DEFINITIONS
   );
+
+  const updateCache = (cache, { data }) => {
+    // Fetch the todos from the cache
+    const existings = cache.readQuery({
+      query: GET_MAPPING_DEFINITIONS,
+    });
+
+    // Add the new todo to the cache
+    const newMappingDefinition = data.createMappingDefinition.mappingDefinition;
+    cache.writeQuery({
+      query: GET_MAPPING_DEFINITIONS,
+      data: {
+        mappingDefinitions: [
+          ...existings.mappingDefinitions,
+          newMappingDefinition,
+        ],
+      },
+    });
+  };
+
+  const resetInputs = () => {
+    setSrcFileName("");
+    setDestTableName("");
+  };
+
+  const [
+    createMappingDefinition,
+    { save_loading, save_error },
+  ] = useMutation(CREATE_MAPPING_DEFINITION, {
+    update: updateCache,
+    onCompleted: resetInputs,
+  });
 
   if (loading) return <h2>Loading...</h2>;
   if (error) return <p>ERROR: {error.message}</p>;
@@ -24,10 +48,9 @@ const MappingDefinitions = (props) => {
   return (
     <div>
       test from mapping definitions
-      {data.mappingDefinitions.map((def) => (
+      {resGetMds.mappingDefinitions.map((def) => (
         <div>
-          {def.id}.
-          {def.srcFileName} - {def.destTableName}
+          {def.id}.{def.srcFileName} - {def.destTableName}
         </div>
       ))}
       <input
