9061460d51d64969865fb251da0585500fdf9113
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2020-06-09 19:17:44 -0400

    moved md input to detail page

--- admin/client/src/pages/MappingDefinition.js
+++ admin/client/src/pages/MappingDefinition.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
+import ButtonSaveMD from "../components/ButtonSaveMD";
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
 const MappingDefinition = (props) => {
+  const [srcFileName, setSrcFileName] = useState("");
+  const [destTableName, setDestTableName] = useState("");
 
-    return (
-        <div>
-            test from mapping definition edit
-        </div>
-    );
-}
+  const classes = useStyles();
+
+  const resetInputs = () => {
+    setSrcFileName("");
+    setDestTableName("");
+  };
+
+  return (
+    <div>
+      test from mapping definition edit
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField
+          id="srcFileName"
+          label="Source"
+          value={srcFileName}
+          onChange={(e) => setSrcFileName(e.target.value)}
+        />
+        <TextField
+          id="destTableName"
+          label="Destination"
+          value={destTableName}
+          onChange={(e) => setDestTableName(e.target.value)}
+        />
+
+        <ButtonSaveMD
+          srcFileName={srcFileName}
+          destTableName={destTableName}
+          resetInputs={resetInputs}
+        />
+      </form>
+    </div>
+  );
+};
 
 export default MappingDefinition;
--- admin/client/src/pages/MappingDefinitions.js
+++ admin/client/src/pages/MappingDefinitions.js
@@ -2,18 +2,11 @@ import React, { useState } from "react";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
-import ButtonSaveMD from "../components/ButtonSaveMD";
 
 const MappingDefinitions = (props) => {
-  const [srcFileName, setSrcFileName] = useState("");
-  const [destTableName, setDestTableName] = useState("");
 
   const { loading, error, data: resGetMds } = useQuery(GET_MAPPING_DEFINITIONS);
 
-  const resetInputs = () => {
-    setSrcFileName("");
-    setDestTableName("");
-  };
 
   if (loading) return <h2>Loading...</h2>;
   if (error) return <p>ERROR: {error.message}</p>;
@@ -26,23 +19,6 @@ const MappingDefinitions = (props) => {
           {def.id}.{def.srcFileName} - {def.destTableName}
         </div>
       ))}
-      <input
-        type="text"
-        name="srcFileName"
-        value={srcFileName}
-        onChange={(e) => setSrcFileName(e.target.value)}
-      />
-      <input
-        type="text"
-        name="destTableName"
-        value={destTableName}
-        onChange={(e) => setDestTableName(e.target.value)}
-      />
-      <ButtonSaveMD
-        srcFileName={srcFileName}
-        destTableName={destTableName}
-        resetInputs={resetInputs}
-      />
     </div>
   );
 };
