da4dfcae5230b52919d0618a7364162a5fb5320a
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2020-06-09 19:17:22 -0400

    #21 fixed wrapper

--- admin/client/src/components/Footer.js
+++ admin/client/src/components/Footer.js
@@ -1,10 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 
 export default function Footer(props) {
-    return (
-      <Fragment>
-        footer
-      </Fragment>
-    );
-  }
-  
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
--- admin/client/src/pages/Index.js
+++ admin/client/src/pages/Index.js
@@ -1,22 +1,28 @@
 import React, { Fragment } from 'react';
 import { Router } from '@reach/router';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 import MappingDefinition from './MappingDefinition';
 import MappingDefinitions from './MappingDefinitions';
 import Footer from '../components/Footer';
-import PageContainer from '../components/PageContainer';
 // import { Footer, PageContainer } from '../components';
 
 export default function Pages() {
   return (
-    <Fragment>
-      <PageContainer>
+    <Container maxWidth="md">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example
+        </Typography>
         <Router primary={false} component={Fragment}>
           <MappingDefinitions path="/" />
-          <MappingDefinition path="mapping-definitions/:id" />
+          <MappingDefinitions path="mapping-definitions" />
+          <MappingDefinition path="mapping-definition/:id" />
         </Router>
-      </PageContainer>
-      <Footer />
-    </Fragment>
-  );
+        <Footer/>
+      </Box>
+    </Container>
+  )
 }
