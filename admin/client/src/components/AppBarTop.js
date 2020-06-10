774f5ae00a29207bfce8d747804fd6ffad0280f7
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2020-06-10 09:17:29 -0400

    #22 added top app bar

---
+++ admin/client/src/components/AppBarTop.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Link } from "@reach/router";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  menu: {
+    textDecoration: "none",
+    color: "inherit",
+  },
+}));
+
+export default function AppBarTop() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Container maxWidth="md">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              <Link className={classes.menu} to="/mapping-definitions">
+                Mapping
+              </Link>
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
+}
--- admin/client/src/pages/Index.js
+++ admin/client/src/pages/Index.js
@@ -1,30 +1,33 @@
-import React, { Fragment } from 'react';
-import { Router } from '@reach/router';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import React, { Fragment } from "react";
+import { Router } from "@reach/router";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
-import MappingDefinition from './MappingDefinition';
-import MappingDefinitions from './MappingDefinitions';
-import NotFound from './NotFound';
-import Footer from '../components/Footer';
-// import { Footer, PageContainer } from '../components';
+import AppBarTop from "../components/AppBarTop";
+import MappingDefinition from "./MappingDefinition";
+import MappingDefinitions from "./MappingDefinitions";
+import NotFound from "./NotFound";
+import Footer from "../components/Footer";
 
 export default function Pages() {
   return (
-    <Container maxWidth="md">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v4-beta example
-        </Typography>
-        <Router primary={false} component={Fragment}>
-          <MappingDefinitions path="/" />
-          <MappingDefinitions path="mapping-definitions" />
-          <MappingDefinition path="mapping-definition/:id" />
-          <NotFound default />
-        </Router>
-        <Footer/>
-      </Box>
-    </Container>
-  )
+    <Fragment>
+      <AppBarTop />
+      <Container maxWidth="md">
+        <Box my={4}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Create React App v4-beta example
+          </Typography>
+          <Router primary={false} component={Fragment}>
+            <MappingDefinitions path="/" />
+            <MappingDefinitions path="mapping-definitions" />
+            <MappingDefinition path="mapping-definition/:id" />
+            <NotFound default />
+          </Router>
+          <Footer />
+        </Box>
+      </Container>
+    </Fragment>
+  );
 }
