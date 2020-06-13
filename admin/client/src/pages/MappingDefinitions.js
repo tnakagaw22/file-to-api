aa3570df3afe60baeabaa0ad7f324c346004defb
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2020-06-12 21:20:01 -0400

    #29 make mapping definitions page cleaner

--- admin/client/src/pages/MappingDefinitions.js
+++ admin/client/src/pages/MappingDefinitions.js
@@ -2,6 +2,16 @@ import React, { useState } from "react";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import { Link } from "@reach/router";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 const MappingDefinitions = (props) => {
 
@@ -13,13 +23,37 @@ const MappingDefinitions = (props) => {
   return (
     <div>
       test from mapping definitions
-      {resGetMds.mappingDefinitions.map((def) => (
-        <ul>
-          <li>
-          <Link to={`/mapping-definition/${def.id}`}>{def.id}.{def.srcFileName} - {def.destTableName}</Link>
-          </li>
-        </ul>
-      ))}
+      <div>
+        <Link to={`/mapping-definition/`}><AddCircleIcon/></Link>
+      </div>
+
+      <TableContainer component={Paper}>
+      <Table stickyHeader aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell>Src</TableCell>
+            <TableCell>Dest</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {resGetMds.mappingDefinitions.map((def) => (
+            <TableRow key={def.id}>
+              {/* <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell> */}
+              <TableCell><Link to={`/mapping-definition/${def.id}`}><EditIcon/></Link></TableCell>
+              <TableCell>{def.srcFileName}</TableCell>
+              <TableCell>{def.destTableName}</TableCell>
+              <TableCell><DeleteIcon /></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+      
     </div>
   );
 };
