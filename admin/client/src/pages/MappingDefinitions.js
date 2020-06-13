3595ec319339335435b841c3819e51c69cc52984
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2020-06-12 21:22:43 -0400

    format change

--- admin/client/src/pages/MappingDefinitions.js
+++ admin/client/src/pages/MappingDefinitions.js
@@ -2,19 +2,18 @@ import React, { useState } from "react";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import { Link } from "@reach/router";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 
 const MappingDefinitions = (props) => {
-
   const { loading, error, data: resGetMds } = useQuery(GET_MAPPING_DEFINITIONS);
 
   if (loading) return <h2>Loading...</h2>;
@@ -24,36 +23,41 @@ const MappingDefinitions = (props) => {
     <div>
       test from mapping definitions
       <div>
-        <Link to={`/mapping-definition/`}><AddCircleIcon/></Link>
+        <Link to={`/mapping-definition/`}>
+          <AddCircleIcon />
+        </Link>
       </div>
-
       <TableContainer component={Paper}>
-      <Table stickyHeader aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell></TableCell>
-            <TableCell>Src</TableCell>
-            <TableCell>Dest</TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {resGetMds.mappingDefinitions.map((def) => (
-            <TableRow key={def.id}>
-              {/* <TableCell component="th" scope="row">
+        <Table stickyHeader aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell>Src</TableCell>
+              <TableCell>Dest</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {resGetMds.mappingDefinitions.map((def) => (
+              <TableRow key={def.id}>
+                {/* <TableCell component="th" scope="row">
                 {row.name}
               </TableCell> */}
-              <TableCell><Link to={`/mapping-definition/${def.id}`}><EditIcon/></Link></TableCell>
-              <TableCell>{def.srcFileName}</TableCell>
-              <TableCell>{def.destTableName}</TableCell>
-              <TableCell><DeleteIcon /></TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-
-      
+                <TableCell>
+                  <Link to={`/mapping-definition/${def.id}`}>
+                    <EditIcon />
+                  </Link>
+                </TableCell>
+                <TableCell>{def.srcFileName}</TableCell>
+                <TableCell>{def.destTableName}</TableCell>
+                <TableCell>
+                  <DeleteIcon />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
