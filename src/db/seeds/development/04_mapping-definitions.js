4fb64cfe0a25de83c35193930837ae8612e31747
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2020-06-14 17:03:59 -0400

    added fieldMappings in seed

--- src/db/seeds/development/04_mapping-definitions.js
+++ src/db/seeds/development/04_mapping-definitions.js
@@ -5,7 +5,7 @@ exports.seed = async knex => {
 
   // Inserts seed entries
   await knex('dev.mapping_definitions').insert([
-    { id: 1, srcFileName: 'OLR-OneKeyMls', destTableName: 'listings' },
-    { id: 2, srcFileName: 'OLR-OneKeyMls', destTableName: 'buildings' },
+    { id: 1, srcFileName: 'OLR-OneKeyMls', destTableName: 'listings', fieldMappings:'[{"destFieldName": "ListingKey", "mappingType": "Column", "value" : "listingKey"},{"destFieldName": "Status", "mappingType": "Column", "value" : "status"}]' },
+    { id: 2, srcFileName: 'OLR-OneKeyMls', destTableName: 'buildings', fieldMappings: null },
   ]);
 };
