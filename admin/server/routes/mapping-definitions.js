08889b107531e693742f704c0fc3d9d980ad692b
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2020-06-16 08:12:17 -0400

    #34 added add/update/delete endpoint

--- admin/server/routes/mapping-definitions.js
+++ admin/server/routes/mapping-definitions.js
@@ -17,16 +17,13 @@ const mappings = [
     },
   ];
 
-// Gets All users
 router.get('/', (req, res) => {
     // for (const key in req.query) {
     //     console.log(key, req.query[key])
     //   }
-console.log('test')
     res.json(mappings)
 });
 
-// Get Single Page
 router.get('/:id', (req, res) => {
     const mapping = mappings.find(user => user.id === parseInt(req.params.id));
 
@@ -37,5 +34,50 @@ router.get('/:id', (req, res) => {
     }
 });
 
+router.post('/', (req, res) => {
+  const newMapping = {
+    id: mappings.length + 1,
+    srcFileName: req.body.srcFileName,
+    destTableName: req.body.destTableName,
+  };
+
+  if (!newMapping.srcFileName || !newMapping.destTableName) {
+    return res.status(400).json({ msg: 'Please include a srcFileName and destTableName' });
+  }
+
+  mappings.push(newMapping);
+  res.json(mappings);
+});
+
+router.put('/:id', (req, res) => {
+  const found = mappings.some(mapping => mapping.id === parseInt(req.params.id));
+
+  if (found) {
+    const updMapping = req.body;
+    mappings.forEach(mapping => {
+      if (mapping.id === parseInt(req.params.id)) {
+        mapping.srcFileName = updMapping.srcFileName ? updMapping.srcFileName : mapping.srcFileName;
+        mapping.destTableName = updMapping.destTableName ? updMapping.destTableName : mapping.destTableName;
+
+        res.json({ msg: 'Mapping updated', mapping });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No mapping with the id of ${req.params.id}` });
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const found = mappings.some(mapping => mapping.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json({
+      msg: 'Mapping deleted',
+      members: mappings.filter(mapping => mapping.id !== parseInt(req.params.id))
+    });
+  } else {
+    res.status(400).json({ msg: `No mapping with the id of ${req.params.id}` });
+  }
+});
 
 module.exports = router;
