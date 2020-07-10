6f806b4795f9ce85dfb438298f1dae937247ff44
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2020-07-10 18:03:44 -0400

    upload file to volume

--- admin/server/datasources/fileUploadDs.js
+++ admin/server/datasources/fileUploadDs.js
@@ -1,25 +1,32 @@
-const path = require('path');
-const fs = require('fs');
-const Queue  = require('bull');
+const path = require("path");
+const fs = require("fs");
+const Queue = require("bull");
+const logger = require("../lib/logger");
 
-const mappingQueue = new Queue('file mapping', 'redis://127.0.0.1:6379');
+const mappingQueue = new Queue("file mapping", "redis://127.0.0.1:6379");
 
-const saveFileToDisk = (file, fileName) => {
-    let fileNameWithTimestamp = `${path.parse(fileName).name}-${new Date().getTime()}${path.parse(fileName).ext}`; 
+const saveFileToDisk = async (file, fileName) => {
+  let fileNameWithTimestamp = `${
+    path.parse(fileName).name
+  }-${new Date().getTime()}${path.parse(fileName).ext}`;
 
-    var saveTo = path.join(__dirname, `../uploaded-files/${fileNameWithTimestamp}`);
-    var outStream = fs.createWriteStream(saveTo);
-    file.pipe(outStream);
-
-}
+  var saveTo = path.join(
+    __dirname,
+    `../uploaded-files/${fileNameWithTimestamp}`
+  );
+  logger.info(`saving to ${saveTo}`);
+  var outStream = fs.createWriteStream(saveTo);
+  await file.pipe(outStream);
+  logger.info(`finished saving to ${saveTo}`);
+};
 
 const addToQueue = (data) => {
-    console.log('adding to queue')
-    mappingQueue.add(data);
-    console.log('added to queue')
-}
+  console.log("adding to queue");
+  mappingQueue.add(data);
+  console.log("added to queue");
+};
 
 module.exports = {
-    saveFileToDisk,
-    addToQueue
-}
+  saveFileToDisk,
+  addToQueue,
+};
