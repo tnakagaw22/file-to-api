3e54b4ef2204814bc798c270c573432b06394cda
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2020-07-30 11:03:08 -0400

    #64 added mock data

---
+++ upload-processor/services/mockParsedRecords.js
@@ -0,0 +1,10 @@
+const parsedRecords = [
+  { listingKey: "T-345", status: "active", "address\r": "56 Marple Ave\r" },
+  { listingKey: "P-212", status: "pending", "address\r": "33 Poir st" },
+];
+
+
+module.exports = {
+    parsedRecords
+  };
+  
