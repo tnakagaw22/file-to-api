83ddb05dbc025c8dbddf8829bea18bcd80efc19b
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2020-07-31 15:40:04 -0400

    insert record if no identifiers are specified

--- upload-processor/services/db.js
+++ upload-processor/services/db.js
@@ -3,6 +3,10 @@ const { getDbContext } = require("../lib/db");
 const dbContext = getDbContext(config.database);
 
 const getFirstOne = async (clientCode, tableName, condition) => {
+  if (Object.keys(condition).length <= 0) {
+    return null;
+  }
+  
   return await dbContext(tableName).withSchema(clientCode).where(condition).first();
 };
 
