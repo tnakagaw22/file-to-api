ac6f6b30ccca3eda3ccfbc0f7ac14516d63dc23f
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2020-07-31 12:02:10 -0400

    added import update test

--- upload-processor/services/importer.test.js
+++ upload-processor/services/importer.test.js
@@ -2,27 +2,43 @@ jest.mock("./fileParser");
 jest.mock("./db");
 
 const { parseFile } = require("./fileParser");
-const { insert, update } = require("./db");
+const { getFirstOne, insert, update } = require("./db");
 const { importToDb } = require("./importer");
 
 const { msgContent } = require("./mockMsgContent");
 const { parsedRecords } = require("./mockParsedRecords");
 
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
 describe("importer.importToDb", () => {
-  jest.mock("./fileParser", () => ({
-    parseFile: jest.fn(),
-  }));
+  // jest.mock("./fileParser", () => ({
+  //   parseFile: jest.fn(),
+  // }));
 
-  jest.mock("./db", () => ({
-    insert: jest.fn(),
-    update: jest.fn(),
-  }));
+  // jest.mock("./db", () => ({
+  //   getFirstOne: jest.fn(),
+  //   insert: jest.fn(),
+  //   update: jest.fn(),
+  // }));
 
-  test("importTest", async () => {
+  test("when record does not exist, insert", async () => {
     parseFile.mockImplementation(async () => parsedRecords);
+    getFirstOne.mockImplementation(async () => null)
 
     await importToDb(msgContent, ",");
 
     expect(insert.mock.calls.length).toBe(1);
+  });
+
+  test("when record exists, update", async () => {
+    parseFile.mockImplementation(async () => parsedRecords);
+    getFirstOne.mockImplementation(async () => new Object())
+
+    await importToDb(msgContent, ",");
+ 
+    expect(insert.mock.calls.length).toBe(0);
+    expect(update.mock.calls.length).toBe(2);
   });
 });
