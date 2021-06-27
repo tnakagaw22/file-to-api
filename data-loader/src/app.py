ed9fa8fe19dd08aae9425001948e0b9db793f9bd
tomo <tnakagaw22@hotmail.com>
2021-06-27 17:41:02 -0400

    python data loader app.py with docker

---
+++ data-loader/src/app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+# import sqlalchemy
+
+from datastore import get_mapping_config, save_mapped_data
+
+
+def load_file(file_path, mapping_name):
+    mapping_config = get_mapping_config(mapping_name)
+    column_mappings = mapping_config['column_mappings']
+    dest_source_mappings = dict((v, k) for k, v in column_mappings.items())
+
+    df = pd.read_csv(file_path)
+
+    print('mapping config', mapping_config)
+    print('data', df.head(5))
+
+
+load_file('src/data/hgmls_multi.csv', 'hgmls_multi')
